a096416244b6eb81c1e08aefd82423e28a8d4dfd
unknown <sripadvai@gmail.com>
2021-08-05 16:38:37 +0530

    first commit

---
+++ tet.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "RS1" {
+  name     = "FirstRS1"
+  location = "US - West"
+}
+
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "Vnet1" {
+  name                = "Firstvnet1"
+  resource_group_name = azurerm_resource_group.RS1.name
+  location            = azurerm_resource_group.RS1.location
+  address_space       = ["10.0.0.0/16"]
+}
